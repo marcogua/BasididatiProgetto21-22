850a085cb23219653218e315ec1e73eb0067f6be
mrvittoriosomma <vittoriosomma21@gmail.com>
2022-05-18 20:08:15 +0200

    Aggiustati i nomi di alcune colonne nelle tabelle

--- TracciamentoCovid.sql
+++ TracciamentoCovid.sql
@@ -1,53 +1,55 @@
 --Creazione del database
 CREATE DATABASE TracciamentoCovidRistoranti;
 
+--Creazione delle tabelle
+
 --Creazione tabella CLIENTE
 CREATE TABLE cliente(
 nome character varying(64),
 cognome character varying(64),
-numeroCartaIdentita character varying(8),
-numeroTelefono character varying(10)
+numero_carta_identita character varying(8),
+numero_telefono character varying(10)
 );
 
 --Creazione tabella TAVOLATA
 CREATE TABLE tavolata(
-orarioArrivo date,
-elencoCamerieri character varying(64),
-elencoPersoneTavolo character varying(64)
+orario_arrivo date,
+elenco_personale character varying(64),
+elenco_persone_tavolo character varying(64)
 );
 
 --Creazione tabella RISTORANTE
 CREATE TABLE ristorante(
-nomeRistorante character varying(64),
+nome_ristorante character varying(64),
 proprietario character varying(64),
 indirizzo character varying(64),
 civico character varying(5),
 cap integer,
-pIva character varying(16)
+numero_partita_iva character varying(11)
 );
 
 --Creazione tabella SALA
 CREATE TABLE sala(
-codiceSala integer,
-nomeSala character varying(64),
-numeroTavoli integer
+codice_sala integer,
+nome_sala character varying(64),
+numero_tavoli integer
 );
 
 --Creazione tabella TAVOLO
 CREATE TABLE tavolo(
-codiceTavolo integer,
-numeroMaxPersone integer
+codice_tavolo integer,
+numero_massimo_persone integer
 );
 
 --Creazione tabella PERSONALE
 CREATE TABLE personale(
 nome character varying(64),
 congome character varying(64),
-telefono character varying(10),
-ruolo character varying(64)
+numero_telefono character varying(10),
+mansione character varying(64)
 );
 
 --Creazione tabella THEDIVISION ovvero persona infetta
 CREATE TABLE theDivision(
-statoCovid19 boolean
+stato_Covid19 boolean
 );
