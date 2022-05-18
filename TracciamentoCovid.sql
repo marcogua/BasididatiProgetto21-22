8f7ccd4f6a4fe85fecb9d07331f9467aea580340
mrvittoriosomma <vittoriosomma21@gmail.com>
2022-05-18 13:22:42 +0200

    Creazione delle tabelle: Ristorante-Sala-Tavola-Tavolata-Personale-Cliente-TheDivision

--- TracciamentoCovid.sql
+++ TracciamentoCovid.sql
@@ -1,2 +1,53 @@
-//Creazione del database
+--Creazione del database
 CREATE DATABASE TracciamentoCovidRistoranti;
+
+--Creazione tabella CLIENTE
+CREATE TABLE cliente(
+nome character varying(64),
+cognome character varying(64),
+numeroCartaIdentita character varying(8),
+numeroTelefono character varying(10)
+);
+
+--Creazione tabella TAVOLATA
+CREATE TABLE tavolata(
+orarioArrivo date,
+elencoCamerieri character varying(64),
+elencoPersoneTavolo character varying(64)
+);
+
+--Creazione tabella RISTORANTE
+CREATE TABLE ristorante(
+nomeRistorante character varying(64),
+proprietario character varying(64),
+indirizzo character varying(64),
+civico character varying(5),
+cap integer,
+pIva character varying(16)
+);
+
+--Creazione tabella SALA
+CREATE TABLE sala(
+codiceSala integer,
+nomeSala character varying(64),
+numeroTavoli integer
+);
+
+--Creazione tabella TAVOLO
+CREATE TABLE tavolo(
+codiceTavolo integer,
+numeroMaxPersone integer
+);
+
+--Creazione tabella PERSONALE
+CREATE TABLE personale(
+nome character varying(64),
+congome character varying(64),
+telefono character varying(10),
+ruolo character varying(64)
+);
+
+--Creazione tabella THEDIVISION ovvero persona infetta
+CREATE TABLE theDivision(
+statoCovid19 boolean
+);
