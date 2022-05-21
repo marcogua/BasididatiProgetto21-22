bdb61f888aba3053789b77cd323c774b2d3ae204
mrvittoriosomma <vittoriosomma21@gmail.com>
2022-05-21 10:32:25 +0200

    Aggiunte le classi PERSONALE e AVVENTORE iniziato la popolazione della tabella RISTORANTE

--- TracciamentoCovid.sql
+++ TracciamentoCovid.sql
@@ -5,36 +5,52 @@ CREATE DATABASE TracciamentoCovidRistoranti;
 
 --Enumerazione per lo stato del persona che efettua la segnalazione
 CREATE DOMAIN stato AS character varying(8)
-CHECK(
-    VALUE ~ 'Positivo'
-    OR VALUE ~ 'Negativo'
+    CHECK(
+        VALUE ~ 'Positivo'
+        OR VALUE ~ 'Negativo'
+);
+
+--Enumerazione per la tipologia di mansioni
+CREATE DOMAIN mansione AS character varying(16)
+    CHECK(
+        VALUE ~ 'Barman'
+        OR VALUE ~ 'Cameriere'
+        OR VALUE ~ 'Cuoco'
+        OR VALUE ~ 'DirettoreDiSala'
 );
 
 --Creazione delle tabelle
 
 --Creazione tabella RISTORANTE
 CREATE TABLE ristorante(
-nome character varying(64) NOT NULL,
-via character varying(64) NOT NULL,
-civico character varying(5) NOT NULL,
-cap character varying(5) NOT NULL,
-p_iva character varying(11) NOT NULL UNIQUE,
-nome_proprietario character varying(64) NOT NULL
+    nome character varying(255) NOT NULL,
+    via character varying(255) NOT NULL,
+    civico character varying(5) NOT NULL,
+    cap character varying(5) NOT NULL,
+    p_iva character varying(11) NOT NULL UNIQUE,
+    nome_proprietario character varying(255) NOT NULL
 );
 
 --Creazione tabella SALA
 CREATE TABLE sala(
-codice_sala character varying(4) NOT NULL UNIQUE,
-nome_sala character varying(64) NOT NULL,
-numero_tavoli integer NOT NULL,
-p_iva character varying(11) NOT NULL UNIQUE
+    codice_sala character varying(12) NOT NULL UNIQUE,
+    nome_sala character varying(255) NOT NULL,
+    numero_tavoli integer NOT NULL,
+    p_iva character varying(11) NOT NULL UNIQUE
 );
 
 --Creazione tabella TAVOLA
 CREATE TABLE tavola(
-numero_tavola character varying(4) NOT NULL UNIQUE,
-numero_persone_max integer NOT NULL,
-codice_sala character varying(12) NOT NULL UNIQUE
+    numero_tavola character varying(12) NOT NULL UNIQUE,
+    numero_persone_max integer NOT NULL,
+    codice_sala character varying(12) NOT NULL UNIQUE
+);
+
+--Creazione tabella TAVOLATA
+CREATE TABLE tavolata(
+    id_tavolata character varying(12) NOT NULL UNIQUE,
+    orario_arrivo date,
+    nt character varying(12) NOT NULL UNIQUE
 );
 
 --Creazione tabella VICINANZA
@@ -43,29 +59,31 @@ CREATE TABLE vicinanza(
     nts character varying(12) NOT NULL UNIQUE
 );
 
---Creazione tabella TAVOLATA
-CREATE TABLE tavolata(
-id_tavolata character varying(12) NOT NULL UNIQUE,
-elenco_persone character varying(64),
-orario_arrivo date,
-elenco_camerieri character varying(64),
-nt character varying(12) NOT NULL UNIQUE
-);
-
 --Creazione tabella SEGNALAZIONE
 CREATE TABLE segnalazione(
-id_segnalazione character varying(64) NOT NULL UNIQUE,
-stato_Covid stato NOT NULL,
-data_segnalazione date NOT NULL,
-tavolata character varying(12) NOT NULL UNIQUE
+    id_segnalazione character varying(64) NOT NULL UNIQUE,
+    stato_Covid stato NOT NULL,
+    data_segnalazione date NOT NULL,
+    tavolata character varying(12) NOT NULL UNIQUE
 );
 
 --Creazione tabella PERSONA
 CREATE TABLE persona(
-nome character varying(64) NOT NULL,
-cognome character varying(64) NOT NULL,
-telefono character varying(10) NOT NULL UNIQUE,
-id_tavolata character varying(12) NOT NULL UNIQUE
+    nome character varying(64) NOT NULL,
+    cognome character varying(64) NOT NULL,
+    telefono character varying(10) NOT NULL UNIQUE,
+    id_tavolata character varying(12) NOT NULL UNIQUE
+);
+
+--Creazione tabella PERSONALE
+CREATE TABLE personale(
+    numero_opt integer NOT NULL UNIQUE,
+    mansione mansione NOT NULL
+);
+
+--Creazione tabella AVVENTORE
+CREATE TABLE avventore(
+    numero_carta_identita character varying(8)
 );
 
 --Creazione delle chiavi primarie e chiavi esterne
@@ -124,3 +142,23 @@ ALTER TABLE persona
 --Creazione dei principali check
 
 --Creazione dei trigger
+
+--Popolazione del database
+
+--Popolazione della tabella RISTORANTE
+INSERT INTO ristorante
+VALUES('Pizzeria da Mario','Via Mario Rssi', '33', '80067', '925837209382', 'Mario Giordano');
+INSERT INTO ristorante
+VALUES('Ristorante le 4 terre','Via Saviano Superiore', '21', '80066', '925837209391', 'Luca Marcato');
+INSERT INTO ristorante
+VALUES('Ristorante La Viola','Via Carlo II', '11', '80033', '296402817640', 'Antonio Esposito');
+INSERT INTO ristorante
+VALUES('Ristorante AmoreEterno','Via Sulmona', '7', '80011', '296402817611', 'Maria Sala');
+INSERT INTO ristorante
+VALUES('Pizzeria La Scura','Via Enrico Fermi', '203', '00012', '902754019286', 'Martina Sultano');
+INSERT INTO ristorante
+VALUES('Ristorante La Viola','Via Carlo II', '11', '80033', '296402817640', 'Antonio Esposito');
+INSERT INTO ristorante
+VALUES('Ristorante Da Carola','Via Don Giovanni', '10', '80001', '091287528016', 'Maria De Falco');
+INSERT INTO ristorante
+VALUES('Pizzeria La Dalila','Via Carlo Salvo', '909', '60098', '109982085671', 'Marco Salvato');
