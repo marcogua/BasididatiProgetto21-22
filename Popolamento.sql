cfeae6104ef4ed90d501572b3f067c15ae3f3435
mrvittoriosomma <vittoriosomma21@gmail.com>
2022-05-30 14:25:45 +0200

    Modificato il numero carta identia ora è a 9 caratteri rimosso lo stato nella segnalazione in quanto la segnalazione è solo per un caso positivo inoltre popolato il database per i tavoli e le persone

--- Popolamento.sql
+++ Popolamento.sql
@@ -1,89 +1,624 @@
 --Popolazione del database
 
 --Popolazione della tabella RISTORANTE
-INSERT INTO ristorante
+INSERT INTO Ristorante
     VALUES('Pizzeria da Mario','Via Mario Rssi', '33', '80067', '92583720938', 'Mario Giordano');
 
-INSERT INTO ristorante
+INSERT INTO Ristorante
     VALUES('Ristorante le 4 terre','Via Saviano Superiore', '21', '80066', '25837209391', 'Luca Marcato');
 
-INSERT INTO ristorante
+INSERT INTO Ristorante
     VALUES('Ristorante La Viola','Via Carlo II', '11', '80033', '96402817640', 'Antonio Esposito');
 
-INSERT INTO ristorante
+INSERT INTO Ristorante
     VALUES('Ristorante AmoreEterno','Via Sulmona', '7', '80011', '29640281761', 'Maria Sala');
 
-INSERT INTO ristorante
+INSERT INTO Ristorante
     VALUES('Pizzeria La Scura','Via Enrico Fermi', '203', '00012', '90275401928', 'Martina Sultano');
 
-INSERT INTO ristorante
+INSERT INTO Ristorante
     VALUES('La Fornacella','Vico Equenze', '149', '80033', '23904568127', 'Antonio Crimaldi');
 
-INSERT INTO ristorante
+INSERT INTO Ristorante
     VALUES('Ristorante Da Carola','Via Don Giovanni', '10', '80001', '09128752801', 'Maria De Falco');
 
-INSERT INTO ristorante
+INSERT INTO Ristorante
     VALUES('Pizzeria La Dalila','Via Carlo Salvo', '909', '60098', '10998208567', 'Marco Salvato');
-
---Non funzionano perchè la partita iva è a 11 caratteri qui c'è ne sono 12
 
 --Popolazione della tabella SALA
 INSERT INTO Sala
-    VALUES(,'Sala Rossa', 13, '25837209391');
+    VALUES(1,'Sala Rossa', 5, '25837209391');
 
 INSERT INTO Sala
-    VALUES(,'Sala Gialla', 18, '25837209391');
+    VALUES(2,'Sala Gialla', 10, '25837209391');
 
 INSERT INTO Sala
-    VALUES(,'Sala Blu', 12, '25837209391');
+    VALUES(3,'Sala Blu', 4, '25837209391');
 
 INSERT INTO Sala
-    VALUES(,'Sala Rosa', 12, '25837209391');
+    VALUES(4,'Sala Rosa', 4, '25837209391');
 
 INSERT INTO Sala
-    VALUES(,'Sala Verde', 8, '25837209391');
+    VALUES(5,'Sala Verde', 8, '25837209391');
 
 INSERT INTO Sala
-    VALUES(,'Sala Magnum', 22, '25837209391');
+    VALUES(6,'Sala Magnum', 6, '25837209391');
 
 INSERT INTO Sala
-    VALUES(,'Londra', 8, '96402817640');
+    VALUES(7,'Londra', 8, '96402817640');
 
 INSERT INTO Sala
-    VALUES(,'Bruxel', 22, '96402817640');
+    VALUES(8,'Bruxel', 4, '96402817640');
 
 INSERT INTO Sala
-    VALUES(,'Prima', 22, '29640281761');
+    VALUES(9,'Prima', 10, '29640281761');
 
 INSERT INTO Sala
-    VALUES(,'Seconda', 8, '29640281761');
+    VALUES(10,'Seconda', 8, '29640281761');
 
 INSERT INTO Sala
-    VALUES(,'Terza', 22, '29640281761');
+    VALUES(11,'Terza', 12, '29640281761');
 
 INSERT INTO Sala
-    VALUES(,'B', 22, '90275401928');
+    VALUES(12,'B', 10, '90275401928');
 
 INSERT INTO Sala
-    VALUES(,'D', 8, '90275401928');
+    VALUES(13,'D', 4, '90275401928');
 
 INSERT INTO Sala
-    VALUES(,'C', 22, '90275401928');
+    VALUES(14,'C', 5, '90275401928');
 
 INSERT INTO Sala
-    VALUES(,'Nobel', 12, '23904568127');
+    VALUES(15,'Nobel', 12, '23904568127');
 
 INSERT INTO Sala
-    VALUES(,'Palma', 28, '23904568127');
+    VALUES(16,'Palma', 8, '23904568127');
 
 INSERT INTO Sala
-    VALUES(,'Oscar', 42, '23904568127');
+    VALUES(17,'Oscar', 6, '23904568127');
 
 INSERT INTO Sala
-    VALUES(,'Sala Rossa', 32, '09128752801');
+    VALUES(18,'Sala Rossa', 5, '09128752801');
 
 INSERT INTO Sala
-    VALUES(,'Piccola', 10, '10998208567');
+    VALUES(19,'Piccola', 5, '10998208567');
 
 INSERT INTO Sala
-    VALUES(,'Grande', 19, '10998208567');
+    VALUES(20,'Grande', 10, '10998208567');
+
+--Popolazione della tabella TAVOLA
+
+INSERT INTO Tavola
+    VALUES(1, 1, 1);
+
+INSERT INTO Tavola
+    VALUES(2, 5, 1);
+
+INSERT INTO Tavola
+    VALUES(3, 8, 1);
+
+INSERT INTO Tavola
+    VALUES(4, 9, 1);
+
+INSERT INTO Tavola
+    VALUES(5, 3, 1);
+
+INSERT INTO Tavola
+    VALUES(6, 2, 2);
+
+INSERT INTO Tavola
+    VALUES(7, 4, 2);
+
+INSERT INTO Tavola
+    VALUES(8, 6, 2);
+
+INSERT INTO Tavola
+    VALUES(9, 8, 2);
+
+INSERT INTO Tavola
+    VALUES(10, 6, 2);
+
+INSERT INTO Tavola
+    VALUES(11, 4, 2);
+
+INSERT INTO Tavola
+    VALUES(12, 2, 2);
+
+INSERT INTO Tavola
+    VALUES(13, 4, 2);
+
+INSERT INTO Tavola
+    VALUES(14, 4, 2);
+    
+INSERT INTO Tavola
+    VALUES(15, 5, 2);
+
+INSERT INTO Tavola
+    VALUES(16, 5, 3);
+
+INSERT INTO Tavola
+    VALUES(17, 5, 3);
+
+INSERT INTO Tavola
+    VALUES(18, 5, 3);
+
+INSERT INTO Tavola
+    VALUES(19, 5, 3);
+
+INSERT INTO Tavola
+    VALUES(20, 4, 4);
+
+INSERT INTO Tavola
+    VALUES(21, 4, 4);
+
+INSERT INTO Tavola
+    VALUES(22, 4, 4);
+
+INSERT INTO Tavola
+    VALUES(23, 4, 4);
+
+INSERT INTO Tavola
+    VALUES(24, 4, 5);
+
+INSERT INTO Tavola
+    VALUES(25, 4, 5);
+
+INSERT INTO Tavola
+    VALUES(26, 6, 5);
+
+INSERT INTO Tavola
+    VALUES(27, 6, 5);
+
+INSERT INTO Tavola
+    VALUES(28, 4, 5);
+
+INSERT INTO Tavola
+    VALUES(29, 4, 5);
+
+INSERT INTO Tavola
+    VALUES(30, 2, 5);
+
+INSERT INTO Tavola
+    VALUES(31, 4, 5);
+
+INSERT INTO Tavola
+    VALUES(32, 4, 6);
+
+INSERT INTO Tavola
+    VALUES(33, 4, 6);
+
+INSERT INTO Tavola
+    VALUES(34, 4, 6);
+
+INSERT INTO Tavola
+    VALUES(35, 4, 6);
+
+INSERT INTO Tavola
+    VALUES(36, 4, 6);
+
+INSERT INTO Tavola
+    VALUES(37, 4, 6);
+
+INSERT INTO Tavola
+    VALUES(38, 4, 7);
+
+INSERT INTO Tavola
+    VALUES(39, 6, 7);
+
+INSERT INTO Tavola
+    VALUES(40, 6, 7);
+
+INSERT INTO Tavola
+    VALUES(41, 10, 7);
+
+INSERT INTO Tavola
+    VALUES(42, 12, 7);
+
+INSERT INTO Tavola
+    VALUES(43, 4, 7);
+
+INSERT INTO Tavola
+    VALUES(44, 2, 7);
+
+INSERT INTO Tavola
+    VALUES(45, 2, 7);
+
+INSERT INTO Tavola
+    VALUES(46, 6, 8);
+
+INSERT INTO Tavola
+    VALUES(47, 6, 8);
+
+INSERT INTO Tavola
+    VALUES(48, 6, 8);
+
+INSERT INTO Tavola
+    VALUES(49, 6, 8);
+
+INSERT INTO Tavola
+    VALUES(50, 6, 9);
+
+INSERT INTO Tavola
+    VALUES(51, 4, 9);
+
+INSERT INTO Tavola
+    VALUES(52, 4, 9);
+
+INSERT INTO Tavola
+    VALUES(53, 4, 9);
+
+INSERT INTO Tavola
+    VALUES(54, 6, 9);
+
+INSERT INTO Tavola
+    VALUES(55, 6, 9);
+
+INSERT INTO Tavola
+    VALUES(56, 10, 9);
+
+INSERT INTO Tavola
+    VALUES(57, 10, 9);
+
+INSERT INTO Tavola
+    VALUES(58, 6, 9);
+
+INSERT INTO Tavola
+    VALUES(59, 6, 9);
+
+INSERT INTO Tavola
+    VALUES(60, 6, 10);
+
+INSERT INTO Tavola
+    VALUES(61, 6, 10);
+
+INSERT INTO Tavola
+    VALUES(62, 6, 10);
+
+INSERT INTO Tavola
+    VALUES(63, 6, 10);
+
+INSERT INTO Tavola
+    VALUES(64, 4, 10);
+
+INSERT INTO Tavola
+    VALUES(65, 4, 10);
+
+INSERT INTO Tavola
+    VALUES(66, 4, 10);
+
+INSERT INTO Tavola
+    VALUES(67, 6, 10);
+
+INSERT INTO Tavola
+    VALUES(68, 6, 11);
+
+INSERT INTO Tavola
+    VALUES(69, 6, 11);
+
+INSERT INTO Tavola
+    VALUES(70, 6, 11);
+
+INSERT INTO Tavola
+    VALUES(71, 6, 11);
+
+INSERT INTO Tavola
+    VALUES(72, 6, 11);
+
+INSERT INTO Tavola
+    VALUES(73, 6, 11);
+
+INSERT INTO Tavola
+    VALUES(74, 6, 11);
+
+INSERT INTO Tavola
+    VALUES(75, 6, 11);
+
+INSERT INTO Tavola
+    VALUES(76, 6, 11);
+
+INSERT INTO Tavola
+    VALUES(77, 6, 11);
+
+INSERT INTO Tavola
+    VALUES(78, 6, 11);
+
+INSERT INTO Tavola
+    VALUES(79, 6, 11);
+
+INSERT INTO Tavola 
+    VALUES (80, 8, 12);
+
+INSERT INTO Tavola 
+    VALUES (81, 6, 12);
+
+INSERT INTO Tavola 
+    VALUES (82, 1, 12);
+
+INSERT INTO Tavola 
+    VALUES (83, 4, 12);
+
+INSERT INTO Tavola 
+    VALUES (84, 7, 12);
+
+INSERT INTO Tavola 
+    VALUES (85, 4, 12);
+
+INSERT INTO Tavola 
+    VALUES (86, 7, 12);
+
+INSERT INTO Tavola 
+    VALUES (87, 10, 12);
+
+INSERT INTO Tavola 
+    VALUES (88, 10, 12);
+
+INSERT INTO Tavola 
+    VALUES (89, 1, 12);
+
+INSERT INTO Tavola 
+    VALUES (90, 9, 13);
+
+INSERT INTO Tavola 
+    VALUES (91, 2, 13);
+
+INSERT INTO Tavola 
+    VALUES (92, 7, 13);
+
+INSERT INTO Tavola 
+    VALUES (93, 5, 13);
+
+INSERT INTO Tavola 
+    VALUES (94, 7, 14);
+
+INSERT INTO Tavola 
+    VALUES (95, 3, 14);
+
+INSERT INTO Tavola 
+    VALUES (96, 9, 14);
+
+INSERT INTO Tavola 
+    VALUES (97, 9, 14);
+
+INSERT INTO Tavola 
+    VALUES (98, 7, 14);
+
+INSERT INTO Tavola 
+    VALUES (99, 2, 15);
+
+INSERT INTO Tavola 
+    VALUES (100, 6, 15);
+
+INSERT INTO Tavola 
+    VALUES (101, 10, 15);
+
+INSERT INTO Tavola 
+    VALUES (102, 5, 15);
+
+INSERT INTO Tavola 
+    VALUES (103, 9, 15);
+
+INSERT INTO Tavola 
+    VALUES (104, 6, 15);
+
+INSERT INTO Tavola 
+    VALUES (105, 4, 15);
+
+INSERT INTO Tavola 
+    VALUES (106, 9, 15);
+
+INSERT INTO Tavola 
+    VALUES (107, 2, 15);
+
+INSERT INTO Tavola 
+    VALUES (108, 4, 15);
+
+INSERT INTO Tavola 
+    VALUES (109, 9, 15);
+
+INSERT INTO Tavola 
+    VALUES (110, 8, 15);
+
+INSERT INTO Tavola 
+    VALUES (111, 9, 16);
+
+INSERT INTO Tavola 
+    VALUES (112, 2, 16);
+
+INSERT INTO Tavola 
+    VALUES (113, 7, 16);
+
+INSERT INTO Tavola 
+    VALUES (114, 3, 16);
+
+INSERT INTO Tavola 
+    VALUES (115, 4, 16);
+
+INSERT INTO Tavola 
+    VALUES (116, 4, 16);
+
+INSERT INTO Tavola 
+    VALUES (117, 10, 16);
+
+INSERT INTO Tavola 
+    VALUES (118, 9, 16);
+
+INSERT INTO Tavola 
+    VALUES (119, 6, 17);
+
+INSERT INTO Tavola 
+    VALUES (120, 8, 17);
+
+INSERT INTO Tavola 
+    VALUES (121, 5, 17);
+
+INSERT INTO Tavola 
+    VALUES (122, 3, 17);
+
+INSERT INTO Tavola 
+    VALUES (123, 4, 17);
+
+INSERT INTO Tavola 
+    VALUES (124, 4, 17);
+
+INSERT INTO Tavola 
+    VALUES (125, 6, 18);
+
+INSERT INTO Tavola 
+    VALUES (126, 1, 18);
+
+INSERT INTO Tavola 
+    VALUES (127, 4, 18);
+
+INSERT INTO Tavola 
+    VALUES (128, 1, 18);
+
+INSERT INTO Tavola 
+    VALUES (129, 5, 18);
+
+INSERT INTO Tavola 
+    VALUES (130, 7, 19);
+
+INSERT INTO Tavola 
+    VALUES (131, 3, 19);
+
+INSERT INTO Tavola 
+    VALUES (132, 1, 19);
+
+INSERT INTO Tavola 
+    VALUES (133, 8, 19);
+
+INSERT INTO Tavola 
+    VALUES (134, 9, 19);
+
+INSERT INTO Tavola 
+    VALUES (135, 8, 20);
+
+INSERT INTO Tavola 
+    VALUES (136, 9, 20);
+
+INSERT INTO Tavola 
+    VALUES (137, 10, 20);
+
+INSERT INTO Tavola 
+    VALUES (138, 9, 20);
+
+INSERT INTO Tavola 
+    VALUES (139, 5, 20);
+
+INSERT INTO Tavola 
+    VALUES (140, 3, 20);
+
+INSERT INTO Tavola 
+    VALUES (141, 9, 20);
+
+INSERT INTO Tavola 
+    VALUES (142, 6, 20);
+
+INSERT INTO Tavola 
+    VALUES (143, 8, 20);
+
+INSERT INTO Tavola 
+    VALUES (144, 9, 20);
+
+--Popolamento tabella Persona
+
+INSERT INTO Persona VALUES ('Lola', 'Sims', '6520744414', '1', 'CA23796FX');
+INSERT INTO Persona VALUES ('Emma', 'Green', '7656232037', '2', 'CA62381JF');
+INSERT INTO Persona VALUES ('Lucy', 'Paz', '1558720463', '3', 'CA92067XO');
+INSERT INTO Persona VALUES ('Jean', 'Howe', '8404430663', '4', 'CA91403EB');
+INSERT INTO Persona VALUES ('Jacob', 'Kent', '2797152314', '5', 'CA40632YD');
+INSERT INTO Persona VALUES ('Bernard', 'Ramos', '5820273414', '6', 'CA06473UJ');
+INSERT INTO Persona VALUES ('Brian', 'Soto', '5203733803', '7', 'CA15638PM');
+INSERT INTO Persona VALUES ('Tyrone', 'Creighton', '4551404801', '8', 'CA93026TL');
+INSERT INTO Persona VALUES ('Mark', 'Toulouse', '9638137179', '9', 'CA03581BC');
+INSERT INTO Persona VALUES ('Elsie', 'Cross', '9395480161', '10', 'CA84976EA');
+INSERT INTO Persona VALUES ('Kelvin', 'Maselli', '5895477828', '11', 'CA91850AL');
+INSERT INTO Persona VALUES ('Myrtle', 'Parker', '3577732366', '12', 'CA05827CG');
+INSERT INTO Persona VALUES ('Thomas', 'Collins', '5692823689', '13', 'CA09867TS');
+INSERT INTO Persona VALUES ('Virginia', 'Wilkerson', '3440815625', '14', 'CA20938AJ');
+INSERT INTO Persona VALUES ('Thomas', 'Collins', '5636805189', '15', 'CA06519CT');
+INSERT INTO Persona VALUES ('Vincent', 'Jolley', '9550380791', '16', 'CA59087JA');
+INSERT INTO Persona VALUES ('Mario', 'Blanton', '3457644477', '17', 'CA19205DA');
+INSERT INTO Persona VALUES ('Constance', 'Palmer', '7146035044', '18', 'CA02934HG');
+INSERT INTO Persona VALUES ('April', 'Scavuzzo', '1518257368', '19', 'CA02384WM');
+INSERT INTO Persona VALUES ('Benjamin', 'Roseman', '9380585804', '20', 'CA14863PW');
+INSERT INTO Persona VALUES ('William', 'Darnstaedt', '3940463528', '21', 'CA72048RX');
+INSERT INTO Persona VALUES ('Wm', 'Skidmore', '2111209787', '22', 'CA30547MH');
+INSERT INTO Persona VALUES ('Rhonda', 'Miller', '7827002856', '23', 'CA51463TQ');
+INSERT INTO Persona VALUES ('June', 'Hopkins', '2886718403', '24', 'CA37516VW');
+INSERT INTO Persona VALUES ('Mary', 'Hunter', '3887573092', '25', 'CA86792HT');
+INSERT INTO Persona VALUES ('Alfonso', 'Bradley', '3642251028', '26', 'CA50732WA');
+INSERT INTO Persona VALUES ('James', 'Velazquez', '6072472377', '27', 'CA24038MI');
+INSERT INTO Persona VALUES ('Phyllis', 'Reeves', '1806073859', '28', 'CA84905LT');
+INSERT INTO Persona VALUES ('Regina', 'Laughary', '6020729480', '29', 'CA12306NV');
+INSERT INTO Persona VALUES ('Jerry', 'Loureiro', '8092566417', '30', 'CA23578RT');
+INSERT INTO Persona VALUES ('Alexandra', 'Baeza', '3584211412', '31', 'CA79382IW');
+INSERT INTO Persona VALUES ('Chris', 'Stroot', '6944849406', '32', 'CA80734ZT');
+INSERT INTO Persona VALUES ('Karen', 'Martin', '9934030329', '33', 'CA37428US');
+INSERT INTO Persona VALUES ('David', 'Sims', '6256826163', '34', 'CA24038TL');
+INSERT INTO Persona VALUES ('Jennifer', 'Kirk', '4353149210', '35', 'CA25938YC');
+INSERT INTO Persona VALUES ('John', 'Stewart', '5214570094', '36', 'CA09423DI');
+INSERT INTO Persona VALUES ('Denise', 'Wells', '4125609595', '37', 'CA86072BP');
+INSERT INTO Persona VALUES ('James', 'Partridge', '5878466080', '38', 'CA93401RC');
+INSERT INTO Persona VALUES ('Nicholas', 'Carson', '6494358432', '39', 'CA10478GK');
+INSERT INTO Persona VALUES ('Elsa', 'Sylla', '7761301720', '40', 'CA94260IG');
+INSERT INTO Persona VALUES ('James', 'Stevens', '9051152368', '41', 'CA95074MJ');
+INSERT INTO Persona VALUES ('Nellie', 'Canfield', '2504441152', '42', 'CA97104HW');
+INSERT INTO Persona VALUES ('Bonnie', 'Montgomery', '8188052270', '43', 'CA21609ZN');
+INSERT INTO Persona VALUES ('Peter', 'Hemberger', '3335217141', '44', 'CA10896FU');
+INSERT INTO Persona VALUES ('George', 'Pesce', '8650741622', '45', 'CA90627WH');
+INSERT INTO Persona VALUES ('Ali', 'Adame', '6303416220', '46', 'CA70532FT');
+INSERT INTO Persona VALUES ('Juan', 'Tinley', '5668257436', '47', 'CA46702HK');
+INSERT INTO Persona VALUES ('Michael', 'Bloxom', '3827282625', '48', 'CA71983PS');
+INSERT INTO Persona VALUES ('Virgil', 'Cowdery', '9534552268', '49', 'CA96705UM');
+INSERT INTO Persona VALUES ('Patricia', 'Harrell', '8006650445', '50', 'CA94816XK');
+INSERT INTO Persona VALUES ('Dawn', 'Geddes', '5232438961', 'NULL', 'CA35621VX');
+INSERT INTO Persona VALUES ('William', 'Neff', '8752759657', 'NULL', 'CA08621AL');
+INSERT INTO Persona VALUES ('Voncile', 'Granby', '9870675701', 'NULL', 'CA28751RV');
+INSERT INTO Persona VALUES ('Ellen', 'Hendryx', '4898343179', 'NULL', 'CA07295YF');
+INSERT INTO Persona VALUES ('Leland', 'Williams', '4426865283', 'NULL', 'CA82096VS');
+INSERT INTO Persona VALUES ('Jack', 'Gentile', '3585018250', 'NULL', 'CA67039VZ');
+INSERT INTO Persona VALUES ('Deborah', 'Shows', '4887770390', 'NULL', 'CA38901EL');
+INSERT INTO Persona VALUES ('Joseph', 'Church', '3232718928', 'NULL', 'CA37289DA');
+INSERT INTO Persona VALUES ('Jerry', 'Peiper', '7004865991', 'NULL', 'CA64593GU');
+INSERT INTO Persona VALUES ('George', 'Bertolini', '5425123542', 'NULL', 'CA54603YB');
+INSERT INTO Persona VALUES ('Patsy', 'Vlashi', '2155246775', 'NULL', 'CA95243QA');
+INSERT INTO Persona VALUES ('Annette', 'Mckenzie', '7610660847', 'NULL', 'CA27856WB');
+INSERT INTO Persona VALUES ('Andy', 'Carson', '3503085677', 'NULL', 'CA62591EI');
+INSERT INTO Persona VALUES ('Jon', 'Campbell', '7183662750', 'NULL', 'CA58941WB');
+INSERT INTO Persona VALUES ('Sarah', 'Axtell', '2725740518', 'NULL', 'CA23650BI');
+INSERT INTO Persona VALUES ('April', 'Hunt', '3616119487', 'NULL', 'CA25413KZ');
+INSERT INTO Persona VALUES ('Gary', 'Pippin', '5607311412', 'NULL', 'CA08613QC');
+INSERT INTO Persona VALUES ('Gary', 'Alleyne', '5106685398', 'NULL', 'CA34729DS');
+INSERT INTO Persona VALUES ('Evangelina', 'Seale', '6222821659', 'NULL', 'CA35908FS');
+INSERT INTO Persona VALUES ('Laura', 'Graves', '6411557792', 'NULL', 'CA58967SL');
+INSERT INTO Persona VALUES ('Sabrina', 'Warden', '2505387060', 'NULL', 'CA82546VR');
+INSERT INTO Persona VALUES ('Audrey', 'Poole', '3397675909', 'NULL', 'CA16258TW');
+INSERT INTO Persona VALUES ('Devona', 'Starr', '4225640505', 'NULL', 'CA16485ME');
+INSERT INTO Persona VALUES ('William', 'Reardon', '9340377345', 'NULL', 'CA65217IZ');
+INSERT INTO Persona VALUES ('Bruce', 'Brewer', '2945822685', 'NULL', 'CA76294TL');
+INSERT INTO Persona VALUES ('Diane', 'Diaz', '9180431581', 'NULL', 'CA81903QC');
+INSERT INTO Persona VALUES ('Robert', 'Reed', '7230184499', 'NULL', 'CA31925ES');
+INSERT INTO Persona VALUES ('Charlotte', 'Blevins', '9864614347', 'NULL', 'CA68907GQ');
+INSERT INTO Persona VALUES ('Marie', 'Ruiz', '1045734778', 'NULL', 'CA46029TL');
+INSERT INTO Persona VALUES ('Ray', 'Mcgarry', '2164719345', 'NULL', 'CA46539QK');
+INSERT INTO Persona VALUES ('Amanda', 'Carmody', '5345342128', 'NULL', 'CA57320OW');
+INSERT INTO Persona VALUES ('Mattie', 'Cook', '7820128726', 'NULL', 'CA84075UE');
+INSERT INTO Persona VALUES ('Amanda', 'Burgess', '4912217200', 'NULL', 'CA75468BZ');
+INSERT INTO Persona VALUES ('Ruth', 'Holley', '1985704867', 'NULL', 'CA34875YG');
+INSERT INTO Persona VALUES ('Jean', 'Pierre', '3296542911', 'NULL', 'CA82409YU');
+INSERT INTO Persona VALUES ('Robert', 'Pixler', '1191709076', 'NULL', 'CA31687RV');
+INSERT INTO Persona VALUES ('Christopher', 'Moses', '5624542065', 'NULL', 'CA94726BT');
+INSERT INTO Persona VALUES ('Michael', 'Godines', '6414420419', 'NULL', 'CA78530JI');
+INSERT INTO Persona VALUES ('Eric', 'Murray', '3463177098', 'NULL', 'CA50396MN');
+INSERT INTO Persona VALUES ('Ricky', 'Net', '6598000611', 'NULL', 'CA26134JK');
+INSERT INTO Persona VALUES ('Jimmie', 'Burns', '5571153084', 'NULL', 'CA83107AL');
+INSERT INTO Persona VALUES ('Ashley', 'Nobles', '6392726413', 'NULL', 'CA72816MH');
+INSERT INTO Persona VALUES ('Lawrence', 'Gilligan', '3130430607', 'NULL', 'CA41826XC');
+INSERT INTO Persona VALUES ('Mary', 'Herrera', '9997383230', 'NULL', 'CA67201DE');
+INSERT INTO Persona VALUES ('Beverly', 'Cooper', '7156519164', 'NULL', 'CA59613SI');
+INSERT INTO Persona VALUES ('Richard', 'Long', '8763852686', 'NULL', 'CA25849QW');
+INSERT INTO Persona VALUES ('Janice', 'Lucas', '9677314549', 'NULL', 'CA05163KS');
+INSERT INTO Persona VALUES ('Barbara', 'Anderson', '8261623788', 'NULL', 'CA30579IF');
+INSERT INTO Persona VALUES ('Wendy', 'Stapleton', '8655803068', 'NULL', 'CA41796FZ');
+INSERT INTO Persona VALUES ('Althea', 'Fetter', '8535726924', 'NULL', 'CA59263SG');
