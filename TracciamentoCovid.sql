8bc702707b1d709490ea5941cce65feb4dc4f8ca
Vittorio Somma <vittorio@pop-os.localdomain>
2023-03-25 13:22:31 +0100

    Aggiustati alcuni valori ritenuti non necessari

--- TracciamentoCovid.sql
+++ TracciamentoCovid.sql
@@ -3,13 +3,6 @@
 
 --Creazione delle enumerazioni
 
---Enumerazione per lo stato del persona che efettua la segnalazione
-CREATE DOMAIN stato AS character varying(8)
-    CHECK(
-        VALUE = 'Positivo'
-        OR VALUE = 'Negativo'
-);
-
 --Enumerazione per la tipologia di mansioni
 CREATE DOMAIN mansione AS character varying(16)
     CHECK(
@@ -19,10 +12,12 @@ CREATE DOMAIN mansione AS character varying(16)
         OR VALUE = 'DirettoreDiSala'
 );
 
---Creazione delle tabelle
-
+/*
+*TABELLA: RISTORANTE
+*Crea la tabella e implementa i vincoli principali
+*/
 --Creazione tabella RISTORANTE
-CREATE TABLE ristorante(
+CREATE TABLE RISTORANTE(
 --Nome del ristorante tipo Stringa Not Null
     nome character varying(255) NOT NULL,
 --Nome della via del ristorante tipo Stringa Not Null
@@ -35,9 +30,6 @@ CREATE TABLE ristorante(
     p_iva character varying(11) NOT NULL,
 --Nome del proprietario o dei prorpietari tipo Stringa Not Null
     nomeProprietario character varying(255) NOT NULL,
---Creazione della chiave primaria di ristorante ovvero p_iva(numero di partita iva)
-    CONSTRAINT PK_ristorante
-        PRIMARY KEY(p_iva),
 --Check per controllare che la via contenga solo lettere
     CONSTRAINT CHK_viaRistorante
         CHECK (via ~ '^[A-Za-z ]*$'),
@@ -55,8 +47,17 @@ CREATE TABLE ristorante(
         CHECK (nomeProprietario ~ '^[A-Za-z ]*$')
 );
 
+--Crea il vincolo di chiave primaria
+ALTER TABLE RISTORANTE
+ADD CONSTRAINT PK_ristorante
+    PRIMARY KEY(p_iva);
+
+/*
+*TABELLA: SALA
+*Crea la tabella e implementa i vincoli principali
+*/
 --Creazione tabella SALA
-CREATE TABLE sala(
+CREATE TABLE SALA(
 --Codice univoco che identifica una sala tipo Stringa Not Null Unique
     codiceSala integer NOT NULL DEFAULT 1,
 --Nome della sala tipo Stringa Not Null
@@ -65,15 +66,6 @@ CREATE TABLE sala(
     numeroTavoli integer NOT NULL,
 --Numero di partita iva del ristorante tipo Striga Not Null
     p_iva character varying(11) NOT NULL,
---Creazione della chiva primaria di sala ovvero codiceSala(codice della sala)
-    CONSTRAINT PK_sala
-        PRIMARY KEY(codiceSala),
---Creazione della chiave esterna di sala ovvero p_iva(numero di partita iva) che fa riferimento a ristorante.p_iva(numero partita iva)
-    CONSTRAINT FK_sala
-        FOREIGN KEY(p_iva)
-            REFERENCES ristorante(p_iva)
-                ON UPDATE CASCADE
-                ON DELETE CASCADE,
 --Check per controllare che il nomeSala contenga solo lettere
     CONSTRAINT CHK_nomeSalaSala
         CHECK (nomeSala ~ '^[A-Za-z ]*$'),
@@ -82,6 +74,16 @@ CREATE TABLE sala(
         CHECK (p_iva ~ '^[0-9]*$')
 );
 
+--Cra il vincolo di chiave primaria ed esterna
+ALTER TABLE SALA
+    ADD CONSTRAINT PK_sala
+        PRIMARY KEY(codiceSala),
+    ADD CONSTRAINT FK_sala
+        FOREIGN KEY(p_iva)
+            REFERENCES ristorante(p_iva)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE;
+                
 --Trigger per settare la chiave primaria automaticamente
 CREATE OR REPLACE FUNCTION SalaPK()
     RETURNS TRIGGER
@@ -103,6 +105,10 @@ ON sala
 FOR EACH ROW
 EXECUTE PROCEDURE SalaPK();
 
+/*
+*TABELLA: TAVOLA
+*Crea la tabella tavola e implementa i principali vincoli
+*/
 --Creazione tabella TAVOLA
 CREATE TABLE tavola(
 --Numero che distingue univocamente un tavolo tipo Stringa Not Null Unique
@@ -110,17 +116,18 @@ CREATE TABLE tavola(
 --Numero massimo di persone che possono stare a quel tavolo tipo Integer Not Null
     numeroPersoneMax integer NOT NULL,
 --Codice della sala dove si trova il tavolo tipo Stringa Not Null
-    codiceSala integer NOT NULL,
---Creazione della chiave primaria per la tabella tavola ovvero numeroTavola
-    CONSTRAINT PK_tavola
+    codiceSala integer NOT NULL
+);
+
+--Crea i vincoli di chiave primaria ed esterna
+ALTER TABLE TAVOLA
+    ADD CONSTRAINT PK_tavola
         PRIMARY KEY(numeroTavola),
---Creazione della chiave esterna di tavola ovvero codiceSala che fa riferimento a sala.codiceSala
-    CONSTRAINT FK_tavola
+    ADD CONSTRAINT FK_tavola
         FOREIGN KEY(codiceSala)
             REFERENCES sala(codiceSala)
                 ON UPDATE CASCADE
-                ON DELETE CASCADE
-);
+                ON DELETE CASCADE;
 
 --Trigger per settare la chiave primaria automaticamente
 CREATE OR REPLACE FUNCTION TavolaPK()
@@ -143,28 +150,37 @@ ON tavola
 FOR EACH ROW
 EXECUTE PROCEDURE TavolaPK();
 
+/*
+*TABELLA: VICINANZA
+*Crea la tabella e implementa i principali vincoli
+*/
 --Creazione tabella VICINANZA
-CREATE TABLE vicinanza(
+CREATE TABLE VICINANZA(
 --Numero della tavola corrente tipo Not Null
     ntc integer NOT NULL,
 --Numero della tavola succesiva tipo Stringa Not Null
-    nts integer NOT NULL,
---Creazione della chiave esterna per la tabella vicinanza ovvero ntc che fa riferimento a tavola.numeroTavola
-    CONSTRAINT FK_vicinanza1
+    nts integer NOT NULL
+);
+
+--Crea i vincoli di chiave esterna
+ALTER TABLE VICINANZA
+    ADD CONSTRAINT FK_vicinanza1
         FOREIGN KEY(ntc)
             REFERENCES tavola(numeroTavola)
                 ON UPDATE CASCADE
                 ON DELETE CASCADE,
---Creazione della chiave esterna per la tabella vicinanza ovvero nts che fa riferimento a tavola.numeroTavola
-    CONSTRAINT FK_vicinanza2
+    ADD CONSTRAINT FK_vicinanza2
         FOREIGN KEY(nts)
             REFERENCES tavola(numeroTavola)
                 ON UPDATE CASCADE
-                ON DELETE CASCADE
-);
+                ON DELETE CASCADE;
 
+/*
+*TABELLA: TAVOLATA
+*Crea la tabella e i pricipali vincoli
+*/
 --Creazione tabella TAVOLATA
-CREATE TABLE tavolata(
+CREATE TABLE TAVOLATA(
 --Numero che identifica unicovamente una tavolata tipo Stringa Not Null Unique
     idTavolata integer NOT NULL DEFAULT 1,
 --Data di arrivo della tavolata tipo Date Not Null
@@ -172,17 +188,18 @@ CREATE TABLE tavolata(
 --Oriario di arrivo della tavolta tipo Time Not Null
     orarioArrivo time NOT NULL,
 --Numero che idetifica univocamente la tavola assengata tipo Stringa Not Null
-    numeroTavolata integer NOT NULL,
---Chiave primaria per la tabella tavolata ovvero idTavolata
-    CONSTRAINT PK_tavolata
+    numeroTavolata integer NOT NULL
+);
+
+--Crea i vincoli di chiave primaria ed esterna
+ALTER TABLE TAVOLATA
+    ADD CONSTRAINT PK_tavolata
         PRIMARY KEY(idTavolata),
---Chiave estenra per la tabella tavolata ovvero numeroTavolata che fa riferimento a tavola.numeroTavola
-    CONSTRAINT FK_tavolata
+    ADD CONSTRAINT FK_tavolata
         FOREIGN KEY(numeroTavolata)
             REFERENCES tavola(numeroTavola)
                 ON UPDATE CASCADE
-                ON DELETE CASCADE
-);
+                ON DELETE CASCADE;
 
 --Trigger per settare la chiave primaria automaticamente
 CREATE OR REPLACE FUNCTION TavolataPK()
@@ -205,46 +222,55 @@ ON tavolata
 FOR EACH ROW
 EXECUTE PROCEDURE TavolataPK();
 
+/*
+*TABELLA AVVENTORE
+*Crea la tabella e i pricipali vincoli
+*/
 --Creazione tabella AVVENTORE
-CREATE TABLE avventore(
+CREATE TABLE AVVENTORE(
 --Numero della carta di indentià dell'avventore tipo Stringa Not Null Unique
-    numeroCartaIdentita character varying(8) NOT NULL,
---Creazione della chiave primaria per la tabella avventore ovvero numeroCartaIdentita
-    CONSTRAINT PK_avventore
-        PRIMARY KEY(numeroCartaIdentita),
+    numeroCartaIdentita character varying(9) NOT NULL,
+
 --Check per controllare che il numeriCartaIdentita sia conforme alla carta di indetita nuova
     CONSTRAINT CHK_numeroCartaIdentitaAvventore
-CHECK (numeroCartaIdentita ~ '^[A-Za-z]{2}[0-9]{4}[A-Za-z]{2}.*$')
+CHECK (numeroCartaIdentita ~ '^[A-Za-z]{2}[0-9]{5}[A-Za-z]{2}.*$')
 );
 
+--Crea il vincolo di chiave primaria
+ALTER TABLE AVVENTORE
+    ADD CONSTRAINT PK_avventore
+        PRIMARY KEY(numeroCartaIdentita);
+
+/*
+*TABELLA: SEGNALAZIONE
+*Crea la tabella e i principali vincoli
+*/
 --Creazione tabella SEGNALAZIONE
-CREATE TABLE segnalazione(
+CREATE TABLE SEGNALAZIONE(
 --Numero che identifica unicovamente la sengalazione tipo Stringa Not Null Unique
     idSegnalazione integer NOT NULL DEFAULT 1,
---Stato della persona che ha inviato la sengalazione tipo Stato Not Null
-    statoCovid stato NOT NULL,
 --Data della segnalazione tipo Date Not Null
     dataSegnalazione date NOT NULL,
 --Tavolata a cui ha partecipato la sengalazione tipo Stringa Not Null
     tavolata integer NOT NULL,
 --Persona che ha fatto la segnalazione
-    numeroPersona character varying(8) NOT NULL,
---Creazione della chiave primaria per la tabella segnalazione ovvero idSegnalazione
-    CONSTRAINT PK_segnalazione
+    numeroPersona character varying(9) NOT NULL
+);
+
+--Crea i vincoli di chiave primaria ed esterna
+ALTER TABLE SEGNALAZIONE
+    ADD CONSTRAINT PK_segnalazione
         PRIMARY KEY(idSegnalazione),
---Creazione della chiave esterna per la tabella sengalazione ovvero tavolata che fa riferimento a tavolata.idTavolata
-    CONSTRAINT FK_segnalazione
+    ADD CONSTRAINT FK_segnalazione
         FOREIGN KEY(tavolata)
             REFERENCES tavolata(idTavolata)
                 ON UPDATE CASCADE
                 ON DELETE CASCADE,
---Creazione della seconda chiave esterna per la tabella sengalazione ovvero numeroPersona che fa riferimento a avventore.numeroCartaIdentita
-     CONSTRAINT FK_segnalazione2
+    ADD CONSTRAINT FK_segnalazione2
         FOREIGN KEY(numeroPersona)
             REFERENCES avventore(numeroCartaIdentita)
                 ON UPDATE CASCADE
-                ON DELETE CASCADE
-);
+                ON DELETE CASCADE;
 
 --Trigger per settare la chiave primaria automaticamente
 CREATE OR REPLACE FUNCTION SegnalazionePK()
@@ -253,7 +279,7 @@ AS $$
 DECLARE
     pk segnalazione.idSegnalazione%TYPE;
 BEGIN
-	SELECT MAX(idSegnalazione) + 1 into pk FROM segnalazione;
+	SELECT MAX(idSegnalazione) + 1 into pk FROM sengalazione;
     IF(NEW.idSegnalazione != pk)THEN
         NEW.idSegnalazione := pk;
     END IF;
@@ -265,18 +291,24 @@ CREATE OR REPLACE TRIGGER SegnalazionePK
 BEFORE INSERT
 ON segnalazione
 FOR EACH ROW
-EXECUTE PROCEDURE SengnalazionePK();
+EXECUTE PROCEDURE SegnalazionePK();
 
+/*
+*TABELLA: PERSONALE
+*Crea la tabella e i principali vincoli
+*/
 --Creazione tabella PERSONALE
-CREATE TABLE personale(
+CREATE TABLE PERSONALE(
 --Numero che idetifica univocamente ogni membro del peronale tipo Stringa Not Null
-    numeroOpt integer NOT NULL DEFAULT 1,
+    numeroOpt integer NOT NULL,
 --Mansione assegnata a ogni membro del personale tipo mansione Not Null
-    mansione mansione NOT NULL,
---Creazione della chiave primaria per la tabella personle ovvero numeroOtp
-    CONSTRAINT PK_personale
-        PRIMARY KEY(numeroOpt)
+    mansione mansione
 );
+
+--Crea il vincolo di chiave primaria
+ALTER TABLE PERSONALE
+    ADD CONSTRAINT PK_personale
+        PRIMARY KEY(numeroOpt);
 
 --Trigger per settare la chiave primaria automaticamente
 CREATE OR REPLACE FUNCTION PersonalePK()
@@ -299,8 +331,12 @@ ON personale
 FOR EACH ROW
 EXECUTE PROCEDURE PersonalePK();
 
+/*
+*TABELLA: PERSONA
+*Crea la tabella e i principali vincoli
+*/
 --Creazione tabella PERSONA
-CREATE TABLE persona(
+CREATE TABLE PERSONA(
 --Nome della persona tipo Stringa Not Null
     nome character varying(64) NOT NULL,
 --Cognome della persona tipo Stringa Not Null
@@ -308,21 +344,9 @@ CREATE TABLE persona(
 --Numero di telefono della persona tipo Stringa Not Null Unique
     telefono character varying(10) NOT NULL UNIQUE,
 --Numero che identifica univocamente un mebro del personale tipo Stringa Not Null Unique
-    numeroOpt integer NOT NULL UNIQUE,
+    numeroOpt integer UNIQUE,
 --Numero di carta di identità che identifica univocamente una persona tipo Stringa Not Null Unique
-    numeroCartaIdentita character varying(8) NOT NULL UNIQUE,
---Creazione della chiave esterna per la tabella persona ovvero numeroOtp che fa riferimento a personale.numeroOtp
-    CONSTRAINT FK_persona1
-        FOREIGN KEY(numeroOpt)
-            REFERENCES personale(numeroOpt)
-                ON UPDATE CASCADE
-                ON DELETE CASCADE,
---Creazione della chiave esterna per la tabella persona ovvero numeroCartaIdentita che fa riferimento a avventore.numeroCartaIdentita
-    CONSTRAINT FK_persona2
-        FOREIGN KEY(numeroCartaIdentita)
-            REFERENCES avventore(numeroCartaIdentita)
-                ON UPDATE CASCADE
-                ON DELETE CASCADE,
+    numeroCartaIdentita character varying(9) UNIQUE,
 --Check per controllare che il nome contenga solo lettere
     CONSTRAINT CHK_nomePersona
         CHECK (nome ~ '^[A-Za-z]*$'),
@@ -334,45 +358,92 @@ CREATE TABLE persona(
         CHECK (telefono ~ '^[0-9]*$'),
 --Check per controllare la validita della carta di identita conforme alla nuova
     CONSTRAINT CHK_numeroCartaIdentitaPersona
-        CHECK (numeroCartaIdentita ~ '^[A-Za-z]{2}[0-9]{4}[A-Za-z]{2}.*$')
+        CHECK (numeroCartaIdentita ~ '^[A-Za-z]{2}[0-9]{5}[A-Za-z]{2}.*$')
 );
 
---Creazione tabella SERVITO
-CREATE TABLE servito(
---Numero che identifica univocamente un membro del personale tipo Stringa Not Null
-    numeroOpt integer NOT NULL,
---Numero che identifica univocamente una tavolata tipo Stringa Not Null
-    idTavolata integer NOT NULL,
---Creazione della chiave esterna per la tabella servito ovvero numeroOtp che fa riferimento a personale.numeroOtp
-    CONSTRAINT FK_servito1
+--Crea i vincoli di chiave esterna
+ALTER TABLE PERSONA
+    ADD CONSTRAINT FK_persona1
         FOREIGN KEY(numeroOpt)
             REFERENCES personale(numeroOpt)
                 ON UPDATE CASCADE
                 ON DELETE CASCADE,
---Creazione della chiave esterna per la tabella servito ovvero idTavolata che fa riferimento a tavolata.idTavolata
-    CONSTRAINT FK_servito2
+    ADD CONSTRAINT FK_persona2
+        FOREIGN KEY(numeroCartaIdentita)
+            REFERENCES avventore(numeroCartaIdentita)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE;
+                
+--Trigger per l'inserimento di personale e avventore
+CREATE OR REPLACE FUNCTION InsertPersonaAvventore()
+    RETURNS TRIGGER
+AS $$
+DECLARE
+    BEGIN
+	IF(NEW.numeroOpt IS NOT NULL)THEN
+        	INSERT INTO personale VALUES (NEW.numeroOpt, 'Cuoco');
+        	UPDATE persona SET numeroOpt = NEW.numeroOpt WHERE telefono = OLD.telefono;
+		END IF;
+        INSERT INTO avventore VALUES (NEW.numeroCartaIdentita);
+        UPDATE persona SET numeroCartaIdentita = OLD.numeroCartaIdentita WHERE telefono = OLD.telefono;
+	RETURN NEW;
+    COMMIT;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER InsertPersonaleAvventore
+BEFORE INSERT
+ON persona
+FOR EACH ROW
+EXECUTE PROCEDURE InsertPersonaAvventore();
+
+/*
+*TABELLA: SERVITO
+*Crea la tabella e i pincipali vincoli
+*/
+--Creazione tabella SERVITO
+CREATE TABLE SERVITO(
+--Numero che identifica univocamente un membro del personale tipo Stringa Not Null
+    numeroOpt integer NOT NULL, 
+--Numero che identifica univocamente una tavolata tipo Stringa Not Null
+    idTavolata integer NOT NULL
+);
+
+--Crea i vincoli di chiave esterna
+ALTER TABLE SERVITO
+    ADD CONSTRAINT FK_servito1
+        FOREIGN KEY(numeroOpt)
+            REFERENCES personale(numeroOpt)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE,
+    ADD CONSTRAINT FK_servito2
         FOREIGN KEY(idTavolata)
             REFERENCES tavolata(idTavolata)
                 ON UPDATE CASCADE
-                ON DELETE CASCADE
-);
+                ON DELETE CASCADE;
 
+/*
+*TABELLA PARTECIPA
+*Crea la tabella e i principali vincoli
+*/
 --Creazione tabella PARTECIPA
-CREATE TABLE partecipa(
+CREATE TABLE PARTECIPA(
 --Numero che identifica univocamente una tavolata tipo Strinfa Not Null
     idTavolata integer NOT NULL,
 --Numero carta di identità che identifica univocamente una persona tipo Stringa Not Null
-    numeroCartaIdentita character varying(8) NOT NULL,
---Creazione della chiave esterna per la tabella partecipa ovvero idTavolata che fa riferimento a tavolata.idTavolata
-    CONSTRAINT FK_partecipa1
+    numeroCartaIdentita character varying(9) NOT NULL
+
+);
+
+--Crea i vincoli di chiave esterna
+ALTER TABLE PARTECIPA
+    ADD CONSTRAINT FK_partecipa1
         FOREIGN KEY(idTavolata)
             REFERENCES tavolata(idTavolata)
                 ON UPDATE CASCADE
                 ON DELETE CASCADE,
---Creazione della chiave esterna per la tabella partecipa ovvero numeroCartaIdentita che fa riferimento a avventore.numeroCartaIdentita
-    CONSTRAINT FK_partecipa2
+    ADD CONSTRAINT FK_partecipa2
         FOREIGN KEY(numeroCartaIdentita)
             REFERENCES avventore(numeroCartaIdentita)
                 ON UPDATE CASCADE
-                ON DELETE CASCADE
-);
+                ON DELETE CASCADE;
