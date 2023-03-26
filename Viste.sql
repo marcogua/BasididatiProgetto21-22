99666d7dd7234c8377b55864ef31af9b23fb9424
Marco Guadagno <marcoguadagno1@gmail.com>
2023-03-26 17:49:29 +0200

    Aggiunte viste

--- Viste.sql
+++ Viste.sql
@@ -14,3 +14,26 @@ CREATE OR REPLACE VIEW AvventoriGiornalieri AS
                 ON tavola.codicesala = sala.codicesala) INNER JOIN Ristorante
                     ON sala.p_iva = ristorante.p_iva)
     GROUP BY date_part('day', dataarrivo), ristorante.p_iva; 
+
+
+CREATE OR REPLACE VIEW Totale_persone_per_ristorante AS
+SELECT ristorante.nome, ristorante.p_iva, SUM(foo2.sommapersala) AS totale_per_ristorante
+FROM ristorante INNER JOIN (SELECT SUM(foo.numeropertavolo) AS sommapersala, sala.codicesala, sala.p_iva
+							FROM sala INNER JOIN (SELECT partecipa.idtavolata, COUNT(partecipa.idtavolata) AS numeropertavolo, tavola.codicesala
+									FROM (tavola INNER JOIN tavolata
+	 									ON tavola.numerotavola = tavolata.numerotavolata) INNER JOIN partecipa
+										ON tavolata.idtavolata = partecipa.idtavolata
+									GROUP BY partecipa.idtavolata,tavola.codicesala) AS foo
+								ON sala.codicesala = foo.codicesala
+							GROUP BY sala.codicesala, sala.p_iva) AS foo2
+		ON ristorante.p_iva = foo2.p_iva
+GROUP BY ristorante.p_iva;
+
+CREATE OR REPLACE VIEW somma_tot_persone_posti_ristorante AS
+SELECT ristorante.p_iva, SUM(foo.somma_persone_sale)
+FROM ristorante INNER JOIN (SELECT sala.codicesala, SUM(numeropersonemax) AS somma_persone_sale,sala.p_iva
+							FROM tavola INNER JOIN sala
+								ON sala.codicesala = tavola.codicesala
+							GROUP BY sala.codicesala,sala.p_iva) AS foo
+		ON ristorante.p_iva = foo.p_iva
+GROUP BY ristorante.p_iva;
